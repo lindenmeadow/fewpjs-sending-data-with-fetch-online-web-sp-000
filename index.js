46b19688d464f741f9c1dc610e7d32dd2f9386d7
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-06 20:42:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
